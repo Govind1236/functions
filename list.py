f2378617ee78f0e67d603465e7166aad88568d62
Govind <govindbasnet02@gmail.com>
2024-02-08 22:35:27 +0545

    Initialized

---
+++ list.py
@@ -0,0 +1,9 @@
+list1 = [1 , 2 , 1 ,5 ,1 ,1 ,5 ,1, 1]
+score = 0
+if 1 in list1:
+    score  = list1.count(1)
+    print("yes")
+else:
+    print("no")
+print("The repeated number is:",score)
+score = False
