05e487fe1f6382582610e21a836b5c7a3c079032
Govind <govindbasnet02@gmail.com>
2024-02-09 17:46:38 +0545

    fix: defined f-string utilitie

--- f-string.py
+++ f-string.py
@@ -1,4 +1,4 @@
-name = "i from {0} and i live in {1}"
-cname= "Nepal"
-pname = "Jhapa"
-print(name.format(cname , pname))
+l1 = f"test"
+l2 = f"learn"
+name = f"Hey this is {l1}and  lets {l2} to implement this"
+print(name)
