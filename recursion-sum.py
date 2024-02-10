8c9e1b562ba270a2954b5dd3ab14f29b0a01ad4c
Govind <govindbasnet02@gmail.com>
2024-02-10 20:33:28 +0545

    Feat: sum of number using recursion

---
+++ recursion-sum.py
@@ -0,0 +1,7 @@
+def sum_of_user_input(a , b):
+  
+    return a + b
+add0 = int(input("Enter a number: "))
+add1_ = int(input("Enter second number: "))
+result_ = sum_of_user_input(add0 , add1_)
+print("The result is: ",result_)
