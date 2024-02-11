968ac78165200bbd6c215090cff6282c1403db0d
Govind <govindbasnet02@gmail.com>
2024-02-11 20:50:14 +0545

    Feat: add sets usage

--- sets.py
+++ sets.py
@@ -1,3 +1,12 @@
-set_ = {"Govind", 2 , 8 , 2}
-for result in set_:
-    print(set_)
+# set_ = {"Govind", 2 , 8 , 2}
+# for result in set_:
+#     print(set_)
+# city = {"Govind", "German", "Canada"}
+# city1 = {"Birtamod", "sanischare", "Jhapa"}
+# print(city.union(city1))
+city = {"Govind", "Birtamod", "Canada"}
+city1 = {"Birtamod", "sanischare", "Jhapa"}
+# city.update(city1)
+# print(city.union(city1))
+print(city.intersection(city1))
+# print(city1, city)
