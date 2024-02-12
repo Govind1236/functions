4060e4c5545d51e869da34623a1135162a839211
Govind <govindbasnet02@gmail.com>
2024-02-12 21:09:24 +0545

    Feat: add use of dictionaries

--- dictionaries.py
+++ dictionaries.py
@@ -4,6 +4,15 @@ dic = {
      321 : "Pranamitol"
       
 }
-print(dic[321])
-
-print(dic.get("Govind"))
+# print(dic[321])
+# # to get the data in define key
+# print(dic.get("Govind"))
+# # to get all keys
+# print(dic.keys())
+# # to get value in keys
+# print(dic.values())
+# for info in dic.keys():
+#     print(dic[info])
+print(dic.items())
+for info, value in dic.items():
+    print(f"The value in {info} is {value}")
