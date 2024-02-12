1f543d85ab233c64ed9ee730cc56d7a676a4f9c9
Govind <govindbasnet02@gmail.com>
2024-02-12 20:22:21 +0545

    Feat: add pop

--- sets.py
+++ sets.py
@@ -4,10 +4,14 @@
 # city = {"Govind", "German", "Canada"}
 # city1 = {"Birtamod", "sanischare", "Jhapa"}
 # print(city.union(city1))
-city = {"Govind", "Birtamod", "Canada"}
-city1 = {"Birtamod", "sanischare", "Jhapa"}
+city = {"Govind", "Birtamod", "Canada", "Usa"}
+city1 = {"Paris", "sanischare", "Jhapa" }
+# print(city.intersection_update(city1))
 # city.update(city1)
 # print(city.union(city1))
-# print(city.intersection_update(city1))
-print(city.symmetric_difference(city1))
+# print(city.difference(city1))
 # print(city1, city)
+# print(city.isdisjoint(city1))
+item = city.pop()
+print(city)
+print(item)
