b4bfff301e18466a0c88ab2215615de061c69483
Govind <govindbasnet02@gmail.com>
2024-02-15 22:20:21 +0545

    Feat: add error handling

--- exception-handling.py
+++ exception-handling.py
@@ -1,8 +1,17 @@
-num = int(input("Enter a number: "))
-try:
-   for i in range(5):
-    i = i + 1
-    print(i)
-except:
-   print("invalid Input")
+# try:
+#    num = int(input("Enter a number: "))
+#    for i in range(num):
+#     # i = i + 1
+#     print(i)
+# except:
+#    print("invalid Input")
+# print("Enter valid int not a string")
 
+try:
+   num = int(input("Enter a number: "))
+   a = [5 , "g"]
+   print(a[num])
+except ValueError:
+   print("Enter number is not integer")
+except IndexError:
+   print("index Error")
