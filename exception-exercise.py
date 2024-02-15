5580b0bd84dd4bab3a33ff985cac52ee5203ff95
Govind <govindbasnet02@gmail.com>
2024-02-15 23:05:49 +0545

    Feat: add loop in exception

--- exception-exercise.py
+++ exception-exercise.py
@@ -2,6 +2,7 @@
 # on them. However, you should handle the possibility of division by zero and any other unexpected error
 # s gracefully using exception handling.
 def divison():
+ while True:
     try:
         num = float(input("Enter First Number: "))
         num1 = float(input("Enter Second Number: "))
@@ -14,4 +15,10 @@ def divison():
         print("Error: Division By Zero is not allowed")
     except Exception as b:
         print("Unexpected Error occur", b)
+
+    user_choice = input("Do you want to continue? (yes/no): ").strip().lower()
+    if user_choice == "no":
+      print("The loop Ends here")
+      break
+       
 divison()
