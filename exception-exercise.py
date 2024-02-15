d8146697e8af544a1cdd0e2e2596fecc33925160
Govind <govindbasnet02@gmail.com>
2024-02-15 22:43:27 +0545

    Feat: add exception task

---
+++ exception-exercise.py
@@ -0,0 +1,17 @@
+# Write a Python program that takes two numbers as input from the user and performs division
+# on them. However, you should handle the possibility of division by zero and any other unexpected error
+# s gracefully using exception handling.
+def divison():
+    try:
+        num = float(input("Enter First Number: "))
+        num1 = float(input("Enter Second Number: "))
+
+        divide = num / num1
+        print(divide)
+    except ValueError:
+        print("The value error occured")
+    except ZeroDivisionError:
+        print("Error: Division By Zero is not allowed")
+    except Exception as b:
+        print("Unexpected Error occur", b)
+divison()
