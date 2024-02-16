8ce01f7546034e0e1a5e01ea82032f0dc52fe61c
Govind <govindbasnet02@gmail.com>
2024-02-16 13:30:08 +0545

    Feat: add string to execute program

--- raising-error.py
+++ raising-error.py
@@ -1,6 +1,11 @@
-input = int(input("Enter a number: "))
+# input = int(input("Enter a number: "))
 
-if input < 10 or input > 15:
-    raise ValueError("Choose between 10 to 15")
+# if input < 10 or input > 15:
+#     raise ValueError("Choose between 10 to 15")
     
-print("Execute")
+# print("Execute")
+input = input("Enter a String: ".lower())
+if input == "quit":
+    print("Execute program")
+else:
+    raise ValueError("String error please input correct string: ")
