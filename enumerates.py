54c8ff03349b5db115c88d9d8aade2bf707f8da9
Govind <govindbasnet02@gmail.com>
2024-02-16 23:32:57 +0545

    Feat: add enumerate

---
+++ enumerates.py
@@ -0,0 +1,11 @@
+# name = ["Govind", 902 , "Awesome"]
+# index = 0
+# for letter in name:
+#     if (index == 1):
+#         print("Wow its", letter) 
+#     index += 1
+name = ["Govind", 902 , "Awesome"]
+for index, letter in enumerate(name):
+    if (index == 1):
+     print("Wow its", letter) 
+  
