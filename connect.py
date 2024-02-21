2a076b9eb7454c9c77e6635aac5f6f06eb48ce43
Govind <govindbasnet02@gmail.com>
2024-02-21 13:10:04 +0545

    Fix: import module

---
+++ connect.py
@@ -0,0 +1,6 @@
+def welcome(n):
+    return (f"Welcome {n} you are imorted")
+    
+name = input("Enter a name: ")
+result  = welcome(name)
+print(result)
--- import.py
+++ import.py
@@ -1,4 +1,7 @@
-from math import sqrt, pi
-value_ = int(input("Enter a Number to get square root: "))
-result = sqrt(value_) * pi
-print(dir(result))
+# from math import sqrt, pi
+# value_ = int(input("Enter a Number to get square root: "))
+# result = sqrt(value_) * pi
+# print(dir(result))
+import connect
+name = " "
+connect.welcome(name)
