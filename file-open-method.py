5ce101ab08ee3c1d01f7d52811834c35db5fc379
Govind <govindbasnet02@gmail.com>
2024-02-27 21:52:09 +0545

    Feat: add data

--- file-open-method.py
+++ file-open-method.py
@@ -3,9 +3,15 @@ i = 0
 while True:
     line = f.readline()
     i = i + 1
-    if not line:
+    if not line: 
         break
     m1 = line.split(",")[0]
+    m2 = line.split(",")[1]
+    m3 = line.split(",")[2]
     print(f"The marks of student {i} in subjects {m1}")
+    print(f"The marks of student {i} in subjects {m2}")
+    print(f"The marks of student {i} in subjects {m3}")
+
+    print(line)
         
         
