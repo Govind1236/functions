f2cf9b24612e1e4119032a7e7700fc792ce7c61c
Govind <govindbasnet02@gmail.com>
2024-02-28 20:38:30 +0545

    Fix: add seek method

--- file-open-method.py
+++ file-open-method.py
@@ -1,17 +1,22 @@
-f = open("myfile.txt", 'r')
-i = 0
-while True:
-    line = f.readline()
-    i = i + 1
-    if not line: 
-        break
-    m1 = line.split(",")[0]
-    m2 = line.split(",")[1]
-    m3 = line.split(",")[2]
-    print(f"The marks of student {i} in subjects {m1}")
-    print(f"The marks of student {i} in subjects {m2}")
-    print(f"The marks of student {i} in subjects {m3}")
+# f = open("myfile.txt", 'r')
+# i = 0
+# while True:
+#     line = f.readline()
+#     i = i + 1
+#     if not line: 
+#         break
+#     m1 = line.split(",")[0]
+#     m2 = line.split(",")[1]
+#     m3 = line.split(",")[2]
+#     print(f"The marks of student {i} in subjects {m1}")
+#     print(f"The marks of student {i} in subjects {m2}")
+#     print(f"The marks of student {i} in subjects {m3}")
 
-    print(line)
-        
+#     print(line)
+with open('myfile.txt','r') as f:
+    f.seek(2)
+    file = f.read(2)
+    print(file)
+    
+
         
