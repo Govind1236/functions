0ad766a3a36f9b8202a9e199bbc646c36bc061e7
Govind <govindbasnet02@gmail.com>
2024-03-09 21:12:57 +0545

    fix: add reduce

--- map.py
+++ map.py
@@ -1,14 +1,17 @@
-#MAP 
-def cube(x):
-    return x * x * x
+# #MAP 
+# def cube(x):
+#     return x * x * x
 l = [1 ,4 , 8 , 50 ]
-# new = []
-# for i in l:
-#     new.append(cube(i))
-# print(new)
-print(list(map(cube, l)))
+# # new = []
+# # for i in l:
+# #     new.append(cube(i))
+# # print(new)
+# print(list(map(cube, l)))
 
-# Filter
-def filter_function(a):
-    return a > 3
-print(list(filter(filter_function, l)))
+# # Filter
+# def filter_function(a):
+#     return a > 3
+# print(list(filter(filter_function, l)))
+from functools import reduce
+new = reduce(lambda x , y: x +y,l)
+print(new)
