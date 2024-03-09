926ed9769d45ff61f7947a83798583fd6dea9820
Govind <govindbasnet02@gmail.com>
2024-03-09 21:06:47 +0545

    fix: add filter function

--- map.py
+++ map.py
@@ -1,3 +1,4 @@
+#MAP 
 def cube(x):
     return x * x * x
 l = [1 ,4 , 8 , 50 ]
@@ -6,3 +7,8 @@ l = [1 ,4 , 8 , 50 ]
 #     new.append(cube(i))
 # print(new)
 print(list(map(cube, l)))
+
+# Filter
+def filter_function(a):
+    return a > 3
+print(list(filter(filter_function, l)))
